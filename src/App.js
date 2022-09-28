0f7e80fcd56f1eea4b292f93f6a75f182b4df32d
Albert <luoa@mit.edu>
2022-09-28 02:10:28 -0400

    react skeleton

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        //alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <h2>Hey guys,</h2>
+      
+    </div>
+  );
+}
+
+export default App;
